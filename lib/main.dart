4a5cae493d99a34fc37f825fa421e489c7cd295f
Uswatun <uswatunnnk2101@gmailcom>
2023-11-23 11:08:08 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:shop_app/firebase_options.dart';
+import 'package:shop_app/screens/splash/splash_screen.dart';
+
+import 'routes.dart';
+import 'theme.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'The Flutter Way - Template',
+      theme: AppTheme.lightTheme(context),
+      initialRoute: SplashScreen.routeName,
+      routes: routes,
+    );
+  }
+}
